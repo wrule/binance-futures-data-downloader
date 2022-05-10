b94d7cf5d2edf407e465f24abffe465f2e42779a
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-10 16:15:30 +0800

    ETH数据下载

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { JSDOM } from 'jsdom';
 import { Downloader } from './downloader';
-import xmlBTC from './btc';
+import xmlText from './eth';
 
 async function main() {
-  const document = new JSDOM(xmlBTC).window.document;
+  const document = new JSDOM(xmlText).window.document;
   const list = Array.from(document.querySelectorAll('body > a'))
     .map((item) => item.getAttribute('href'))
     .filter((item) => item?.endsWith('.zip')) as string[];
 
-  const d = new Downloader('BTCUSDT');
+  const d = new Downloader('ETHUSDT');
   d.batchDownload(list, 10, true);
 }
 
