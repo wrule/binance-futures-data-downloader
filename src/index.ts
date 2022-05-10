a500ba6055b26d84b3fa3f0d6ff040710554e41e
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-10 16:03:11 +0800

    成功下载

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,13 @@ import { JSDOM } from 'jsdom';
 import { Downloader } from './downloader';
 import xml1m from './1m';
 
-// console.log(xml1m);
-
 async function main() {
-  // const rsp = await axios.get('https://s3-ap-northeast-1.amazonaws.com/data.binance.vision', {
-  //   params: {
-  //     delimiter: '/',
-  //     prefix: 'data/futures/um/daily/klines/BTCUSDT/1m/',
-  //     // marker: 'data/futures/um/daily/klines/BTCUSDT/1m/BTCUSDT-1m-2021-05-18.zip.CHECKSUM'
-  //   },
-  // });
-
   const document = new JSDOM(xml1m).window.document;
   const list = Array.from(document.querySelectorAll('body > a'))
     .map((item) => item.getAttribute('href'))
     .filter((item) => item?.endsWith('.zip')) as string[];
 
   const d = new Downloader('BTCUSDT');
-  // d.download('https://data.binance.vision/data/futures/um/daily/klines/BTCUSDT/1m/BTCUSDT-1m-2021-05-18.zip', false);
   d.batchDownload(list, 5, true);
 }
 
