0a291462efa90757e8fbc5171a542ea08f0ae406
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-11 10:34:03 +0800

    重新下载数据

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,15 @@ import { load_csv } from '@wrule/ohlcv-utils';
 
 
 async function main() {
-  const list = load_csv('./BTCUSDT/result.csv');
-  console.log(list[0]);
+  // const list = load_csv('./BTCUSDT/result.csv');
+  // console.log(list[0]);
 
-  // const document = new JSDOM(xmlText).window.document;
-  // const list = Array.from(document.querySelectorAll('body > a'))
-  //   .map((item) => item.getAttribute('href'))
-  //   .filter((item) => item?.endsWith('.zip')) as string[];
-
-  // const d = new Downloader('ETHUSDT');
-  // d.batchDownload(list, 10, true);
+  const document = new JSDOM(xmlText).window.document;
+  const list = Array.from(document.querySelectorAll('body > a'))
+    .map((item) => item.getAttribute('href'))
+    .filter((item) => item?.endsWith('.zip')) as string[];
+  const d = new Downloader('BTCUSDT');
+  d.batchDownload(list, 10, false);
 }
 
 main();
